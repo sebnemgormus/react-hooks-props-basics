1af123f8b1e5423d08aa69d38dc2a401f2869a01
Sebnem <sebnemgormus@gmail.com>
2022-02-20 18:42:46 +0300

    commit tree d31771c27c04f879eca37808e9b5826133c519b9

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -2,13 +2,13 @@ import React from "react";
 import BlogContent from "./BlogContent";
 import Comment from "./Comment";
 
-function BlogPost() {
+function BlogPost() { 
   return (
     <div id="blog-post">
       <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
-      <Comment commentText="I agree with this statement. - Angela Merkel" />
-      <Comment commentText="A universal truth. - Noam Chomsky" />
-      <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+      <Comment  numberOfComment={1} commentText="I agree with this statement. - Angela Merkel" />
+      <Comment  numberOfComment={2} commentText="A universal truth. - Noam Chomsky" />
+      <Comment  numberOfComment={3} commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
     </div>
   );
 }
